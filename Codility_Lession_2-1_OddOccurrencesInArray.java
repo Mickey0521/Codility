3b1eacbf0f928885fe9ec9198674a0af0f92b97f
Mickey0521 <smallfriend521@gmail.com>
2017-08-24 21:15:06 +0800

    Create Codility_Lession_2-1_OddOccurrencesInArray.java

---
+++ Codility_Lession_2-1_OddOccurrencesInArray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        
+        // Using the concept of "XOR" (^)
+        // when there is a pair A and B 
+        // A^B will be zero 
+        // A^B^C (where C is not paired), 
+        // then A^B^C = C
+        
+        // special case
+        if(A.length == 0)
+            return 0;
+        
+        int unpaired;
+        unpaired = A[0]; // initial
+        
+        for(int i=1; i< A.length; i++){    
+            unpaired = unpaired ^ A[i]; // xor    
+        }
+        
+        return unpaired; // return the unpaired value
+    }
+}
