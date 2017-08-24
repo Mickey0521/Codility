2eb36fc1919bfa922715571f768547161d78620e
Mickey0521 <smallfriend521@gmail.com>
2017-08-24 22:16:26 +0800

    Create  Codility_Lession_3-2_FrogJmp.java

---
+++ Codility_Lession_3-2_FrogJmp.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int X, int Y, int D) {
+        // write your code in Java SE 8
+        
+        long difference = Y - X;
+        
+        long plus =0; // note: need to decide if to "plus one" or not
+        
+        if( difference % D !=0 ) // using "mod" to decide
+            plus =1;             // if not "perfectly Divisible", then plus one 
+        
+        long hop =0; // number of hops the frog needs to jump
+        
+        hop = difference / D;
+        
+        hop = hop + plus;
+        
+        return (int)hop; // return number of hops (long -> int)
+    }
+}
