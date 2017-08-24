5f3493fb142eef81b8157b5ad9a86c51d69d0b07
Mickey0521 <smallfriend521@gmail.com>
2017-08-24 21:26:27 +0800

    Create Codility_Lession_2-2_CyclicRotation.java

---
+++ Codility_Lession_2-2_CyclicRotation.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+        
+        // Using the concept of "mod" (to make it cyclic)
+        
+        int[] new_array = new int[A.length]; // a new array
+        
+        for(int i=0; i< A.length; i++){
+            int new_position = (i + K) % A.length; // using "mod" to do Cyclic Rotation           
+            new_array[new_position] = A[i]; // put A[i] to the new position
+        }
+        
+        return new_array; // return new array
+    }
+}
