2cf3ccf0b865c7c383e09281b64c329d5108281b
Mickey0521 <smallfriend521@gmail.com>
2017-09-02 20:24:24 +0800

    Create Codility_Lession_6-4_NumberOfDiscIntersections.java

---
+++ Codility_Lession_6-4_NumberOfDiscIntersections.java
@@ -0,0 +1,46 @@
+// note: need to import (to use "Arrays.sort(int[])" )
+import java.util.*;
+
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        
+        // main idea:
+        // 1. store all the "lower points" and "upper points" of the discs
+        // 2. count the intersections (if one upper point > one lower point)
+        
+        // note: use "long" for big numbers (be careful)
+        long[] lower = new long[A.length];
+        long[] upper = new long[A.length];
+        
+        for(int i=0; i<A.length; i++){
+            lower[i] = i - (long)A[i]; // note: lower = center - A[i]
+            upper[i] = i + (long)A[i]; // note: upper = center + A[i]
+        }
+        
+        // "sort" the "lower points" and "upper points"
+        Arrays.sort(upper);
+        Arrays.sort(lower);
+        
+        int intersection = 0; // number of intersections
+        int j=0; // for the lower points
+        
+        // scan the upper points
+        for(int i=0; i<A.length; i++){
+        
+            // for the curent "j" (lower point)
+            while( j < A.length && upper[i] >= lower[j]){
+                    intersection = intersection + j; // add j intersections
+                    intersection = intersection - i; // minus "i" (avoid double count) 
+                    j++;
+            }          
+        }
+        
+        // for the overflow cases
+        if(intersection > 10_000_000)
+            return -1;
+        
+        return intersection; // number of intersections      
+    }
+}
