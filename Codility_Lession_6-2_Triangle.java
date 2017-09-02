18e5c3e75974d9620f7e3f8740e1a66b0e9b5677
Mickey0521 <smallfriend521@gmail.com>
2017-09-02 19:12:34 +0800

    Create Codility_Lession_6-2_Triangle.java

---
+++ Codility_Lession_6-2_Triangle.java
@@ -0,0 +1,26 @@
+// note: need to import (so, we can use "Arrays.sort(int[])")
+import java.util.*;
+
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+
+    // main idea: for any combination (A[i-2], A[i-1], A[i]) 
+    // we just need to check if A[i-2] + A[i-1] > A[i] (important)
+    // note: A[i-2] + A[i-1] is the max possible combination (needed to check)
+
+        // Using "Arrays.sort(int[])"
+        Arrays.sort(A);
+        
+        // note: start from i=2
+        for(int i=2; i< A.length; i++){
+            if((long)A[i-2] + (long)A[i-1] > (long)A[i]) // note: using "long" for overflow cases
+                return 1; 
+            // note: we just need one combination
+        }
+        
+        // otherwise, return 0 (no triangular)
+        return 0;
+    }
+}
