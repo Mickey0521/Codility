34ae6be44958621c291ac558b6d72c624688bbb3
Mickey0521 <smallfriend521@gmail.com>
2017-09-16 04:06:17 +0800

    Create Codility_Lession_9-3_MaxSliceSum_elegant.java

---
+++ Codility_Lession_9-3_MaxSliceSum_elegant.java
@@ -0,0 +1,25 @@
+package MaxSliceSum;
+
+// much elegant solution
+// not using "Math.max( 0, maxEndingHere + A[i])"
+// Instead, using "Math.max( A[i], maxEndingPrevious + A[i] )"
+
+class Solution {
+    public int solution(int[] A) {
+        
+        // initial setting A[0]
+        int maxEndingPrevious = A[0];
+        int maxEndingHere = A[0];
+        int maxSoFar = A[0];
+ 
+        // note: for i=0, it will return A[0] (also for "one element" cases)  
+           
+        for(int i = 1; i < A.length; i++){
+            maxEndingHere = Math.max(A[i], maxEndingPrevious + A[i]); // <--- key point~!!
+            maxEndingPrevious = maxEndingHere;
+            maxSoFar = Math.max(maxSoFar, maxEndingHere); // update the max (be careful)
+        }
+        
+        return maxSoFar; // can be used for "all negative" cases 
+    }
+}
